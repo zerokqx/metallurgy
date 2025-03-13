78d0b4b65435411ecd5e8caec89f0b62c3282070
zerok-cell <swiftkey382@gmail.com>
2025-03-13 17:39:23 +0000

    Prettier configuration

--- src/styledComponents/css/text.stl.ts
+++ src/styledComponents/css/text.stl.ts
@@ -1,14 +1,15 @@
-import styled, { CSSProperties } from 'styled-components'
+import styled from 'styled-components'
 import { baseFont } from '@/styledComponents/css/font.stl.ts'
 import { BaseFontProps } from '@/Types/styledComponents/font.types'
-
+import { CSSProperties } from 'react'
 
 interface ExtendedSpanBlueProps extends BaseFontProps {
-    color?: CSSProperties["color"],
-    height?: CSSProperties["height"]
+    color?: CSSProperties['color']
+    height?: CSSProperties['height']
 }
+
 export const SpanBlue = styled.span<ExtendedSpanBlueProps>`
     color: ${({ color }) => color};
-    ${({ wg, fs }) => baseFont({ fm: "Rubik", wg, fs })}
+    ${({ wg, fs }) => baseFont({ fm: 'Rubik', wg, fs })}
     height: max-content;
 `
