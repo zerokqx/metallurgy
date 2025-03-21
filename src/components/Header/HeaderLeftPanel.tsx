acb8d74be2f3699a4a68605e01bff0cb64c02b8c
zerok-cell <swiftkey382@gmail.com>
2025-03-21 18:35:14 +0300

    Добавлены сепараторы в компонент HeaderLeftPanel и добавлен туда комипонент Avatar

--- src/components/Header/HeaderLeftPanel.tsx
+++ src/components/Header/HeaderLeftPanel.tsx
@@ -4,8 +4,11 @@ import { ContainerFlex } from '@/styledComponents/Containers.stl'
 import logotype from '@/assets/Group 5.svg'
 import { TTheme } from '@/styledComponents/css/theme.stl.ts'
 import ThemeSwitch from '@/components/Header/ThemeSwitch.tsx'
+import SeparatorStyled from '@/styledComponents/SeparatorStyled.stl'
+import { Avatar } from '@/components/ui/avatar'
+import AvatarFallbackStyled from '@/styledComponents/AvatarFallback.stl'
 
-const Logotype = () => {
+const HeaderLeftPanel = () => {
     const theme = useTheme() as TTheme
 
     const LogotypeLocal = styled.img.attrs({ src: logotype })`
@@ -19,13 +22,28 @@ const Logotype = () => {
                 borderRadius: '20px',
                 border: `3px ${theme.border.accent}  solid`,
             }}
+            gap={'20px'}
+            padding={'0 20px 0 10px'}
             justifyContent={'center'}
             alignItems={'center'}
         >
             <LogotypeLocal width={'70px'} height={'70px'} />
+            <SeparatorStyled
+                width={'1px'}
+                height={'20px'}
+                orientation={'vertical'}
+            />
             <ThemeSwitch />
+            <SeparatorStyled
+                width={'1px'}
+                height={'20px'}
+                orientation={'vertical'}
+            />
+            <Avatar>
+                <AvatarFallbackStyled>ZR</AvatarFallbackStyled>
+            </Avatar>
         </ContainerFlex>
     )
 }
 
-export default Logotype
+export default HeaderLeftPanel
---
+++ src/styledComponents/SeparatorStyled.stl.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { Separator } from '@/components/ui/separator'
+import { TSeparatorStyled } from '@/types/styledComponents/SeparatorStyled.types'
+import { size } from '@/styledComponents/css/size.stl'
+import { TTheme } from '@/styledComponents/css/theme.stl'
+
+const SeparatorStyled = styled(Separator)<TSeparatorStyled>`
+    background: ${({ theme }) => (theme as TTheme).icon.primary};
+    ${size}
+`
+export default SeparatorStyled
