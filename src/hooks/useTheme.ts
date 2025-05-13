e630eca2d5cf37b55fe28caa91a99e2f88be551a
Мурад Шахсинов <swiftkey382@gmail.com>
2025-05-13 10:36:26 +0300

    useTheme на persists

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,17 +1,14 @@
-import { useLocalStorage } from '@uidotdev/usehooks'
 import { TStatusTheme } from '@/types/redux/themeSlice.types'
 import { useAppDispatch } from '@/hooks/useRedux'
 import { setStatus } from '@/redux/slices/theme.slice'
 
-export const useTheme = (): [TStatusTheme, (state: TStatusTheme) => void] => {
-    const [state, setState] = useLocalStorage<TStatusTheme>('theme', 'dark')
+export const useTheme = (): [(state: TStatusTheme) => void] => {
     const allowTheme: TStatusTheme[] = ['light', 'dark']
     const dispatch = useAppDispatch()
     const swapTheme = (state: TStatusTheme) => {
         if (allowTheme.includes(state)) {
-            setState(state)
             dispatch(setStatus(state))
         } else throw Error(`Unknown state: ${state}`)
     }
-    return [state, swapTheme]
+    return [swapTheme]
 }
